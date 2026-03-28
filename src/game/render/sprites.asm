d72004a2d876ea697b72b26c4fef6b7b36b8c0ac
RhythmicCarnage <admin@prohibited.tv>
2026-03-28 08:51:32 -0600

    Add authored multicolor tile art

--- src/game/render/sprites.asm
+++ src/game/render/sprites.asm
@@ -40,6 +40,40 @@ draw_sprite_done:
     pop ax
     ret
 
+draw_bitmap8:
+    push ax
+    push bx
+    push cx
+    push dx
+    push si
+    push di
+    push bp
+    mov [glyph_base_x], bx
+    mov [glyph_base_y], dx
+    xor bp, bp
+
+draw_bitmap8_row:
+    cmp bp, 8
+    jae draw_bitmap8_done
+    mov bx, [glyph_base_x]
+    mov dx, [glyph_base_y]
+    add dx, bp
+    call compute_offset
+    mov cx, 8
+    rep movsb
+    inc bp
+    jmp draw_bitmap8_row
+
+draw_bitmap8_done:
+    pop bp
+    pop di
+    pop si
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+
 draw_sprite16_2x:
     push ax
     push bx
--- src/game/render/tiles.asm
+++ src/game/render/tiles.asm
@@ -47,102 +47,13 @@ draw_tile:
     jmp draw_floor_tile
 
 draw_floor_tile:
-    push ax
-    push cx
-    push bp
-    mov cx, 8
-    mov bp, 8
-    mov al, PAL_FLOOR
-    call fill_rect
-    mov al, PAL_FLOOR2
-    push bx
-    push dx
-    add bx, 1
-    add dx, 1
-    call put_pixel
-    pop dx
-    pop bx
-    push bx
-    push dx
-    add bx, 5
-    add dx, 6
-    call put_pixel
-    pop dx
-    pop bx
-    test byte ptr [anim_phase], 1
-    jz floor_tile_done
-    mov al, PAL_CYAN
-    push bx
-    push dx
-    add bx, 2
-    add dx, 2
-    call put_pixel
-    pop dx
-    pop bx
-    push bx
-    push dx
-    add bx, 6
-    add dx, 3
-    call put_pixel
-    pop dx
-    pop bx
-    push bx
-    push dx
-    add bx, 3
-    add dx, 5
-    call put_pixel
-    pop dx
-    pop bx
-floor_tile_done:
-    pop bp
-    pop cx
-    pop ax
+    call get_floor_tile_frame
+    call draw_bitmap8
     ret
 
 draw_wall_tile:
-    push ax
-    push cx
-    push bp
-    push bx
-    push dx
-    mov cx, 8
-    mov bp, 8
-    mov al, PAL_WALL
-    call fill_rect
-    pop dx
-    pop bx
-    push bx
-    push dx
-    mov cx, 8
-    mov bp, 1
-    mov al, PAL_WALL2
-    call fill_rect
-    pop dx
-    pop bx
-    push bx
-    push dx
-    mov cx, 1
-    mov bp, 8
-    mov al, PAL_WALL2
-    call fill_rect
-    pop dx
-    pop bx
-    add bx, 1
-    test byte ptr [anim_phase], 1
-    jz wall_pulse_low
-    add dx, 2
-    mov al, PAL_CYAN
-    jmp wall_pulse_ready
-wall_pulse_low:
-    add dx, 5
-    mov al, PAL_PANEL2
-wall_pulse_ready:
-    mov cx, 6
-    mov bp, 1
-    call fill_rect
-    pop bp
-    pop cx
-    pop ax
+    call get_wall_tile_frame
+    call draw_bitmap8
     ret
 
 draw_shard_tile:
@@ -152,88 +63,49 @@ draw_shard_tile:
     ret
 
 draw_locked_tile:
-    call draw_floor_tile
-    push bx
-    push dx
-    add bx, 1
-    add dx, 1
-    mov cx, 2
-    mov bp, 6
-    test byte ptr [anim_phase], 1
-    jz locked_gate_dim
-    mov al, PAL_RED2
-    jmp locked_gate_color_ready
-locked_gate_dim:
-    mov al, PAL_RED
-locked_gate_color_ready:
-    call fill_rect
-    pop dx
-    pop bx
-    push bx
-    push dx
-    add bx, 5
-    add dx, 1
-    mov cx, 2
-    mov bp, 6
-    test byte ptr [anim_phase], 1
-    jz locked_gate_dim2
-    mov al, PAL_RED2
-    jmp locked_gate_color_ready2
-locked_gate_dim2:
-    mov al, PAL_RED
-locked_gate_color_ready2:
-    call fill_rect
-    pop dx
-    pop bx
-    add dx, 3
-    mov cx, 8
-    mov bp, 1
-    test byte ptr [anim_phase], 1
-    jz locked_gate_bar_dim
-    mov al, PAL_RED
-    jmp locked_gate_bar_ready
-locked_gate_bar_dim:
-    mov al, PAL_RED2
-locked_gate_bar_ready:
-    call fill_rect
+    call get_locked_tile_frame
+    call draw_bitmap8
     ret
 
 draw_open_tile:
-    call draw_floor_tile
-    push bx
-    push dx
-    add bx, 1
-    add dx, 1
-    mov cx, 2
-    mov bp, 6
+    call get_open_tile_frame
+    call draw_bitmap8
+    ret
+
+get_floor_tile_frame:
     test byte ptr [anim_phase], 1
-    jz open_gate_dim
-    mov al, PAL_WHITE
-    jmp open_gate_color_ready
-open_gate_dim:
-    mov al, PAL_GATE
-open_gate_color_ready:
-    call fill_rect
-    pop dx
-    pop bx
-    push bx
-    push dx
-    add bx, 5
-    add dx, 1
-    mov cx, 2
-    mov bp, 6
+    jz get_floor_tile_a
+    mov si, offset tile_floor_b
+    ret
+get_floor_tile_a:
+    mov si, offset tile_floor_a
+    ret
+
+get_wall_tile_frame:
     test byte ptr [anim_phase], 1
-    jz open_gate_dim2
-    mov al, PAL_WHITE
-    jmp open_gate_color_ready2
-open_gate_dim2:
-    mov al, PAL_GATE
-open_gate_color_ready2:
-    call fill_rect
-    pop dx
-    pop bx
-    call get_gate_sprite
-    call draw_sprite8
+    jz get_wall_tile_a
+    mov si, offset tile_wall_b
+    ret
+get_wall_tile_a:
+    mov si, offset tile_wall_a
+    ret
+
+get_locked_tile_frame:
+    test byte ptr [anim_phase], 1
+    jz get_locked_tile_a
+    mov si, offset tile_locked_b
+    ret
+get_locked_tile_a:
+    mov si, offset tile_locked_a
+    ret
+
+get_open_tile_frame:
+    test byte ptr [anim_phase], 1
+    jz get_open_tile_a
+    mov si, offset tile_open_b
+    ret
+get_open_tile_a:
+    mov si, offset tile_open_a
     ret
 
 get_shard_sprite:
